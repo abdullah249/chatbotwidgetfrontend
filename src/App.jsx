d3aa86b51e36045a596d44f72b63335baad932e9
Zeeshan Afzal <zeeshan.afzal.37073@gmail.com>
2025-05-03 19:02:37 +0500

    frontend3

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ChatWidget from './components/ChatWidget'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <ChatWidget />
+    </div>
+  )
+}
+
+export default App 
